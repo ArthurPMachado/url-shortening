339476aaac410b0b1f066203e770e10f240f57f4
Arthur Machado <arthur.machado@iclubs.tech>
2024-05-01 19:47:29 -0300

    feat: add controllers to http module and import http module to app module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common'
+import { CreateClientController } from './controllers/create-client.controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
-@Module({})
+@Module({
+  controllers: [CreateClientController, AuthenticateController],
+})
 export class HttpModule {}
