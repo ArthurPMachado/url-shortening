a865a9d6f967cb66ea49ee2b47c3ea2102b89bc2
Arthur Machado <arthur.machado@iclubs.tech>
2024-05-01 02:19:42 -0300

    feat: create client entity

---
+++ src/domain/enterprise/entities/client.ts
@@ -0,0 +1,55 @@
+import { Entity } from '@/core/entities/entity'
+import { IClientProps } from './interfaces/IClientProps'
+import { Optional } from '@/core/types/optional'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export class Client extends Entity<IClientProps> {
+  static create(
+    props: Optional<IClientProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const client = new Client(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return client
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+    this.touch()
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  set email(email: string) {
+    this.props.email = email
+    this.touch()
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+}
