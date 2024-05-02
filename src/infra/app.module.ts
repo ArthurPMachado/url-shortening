841ec800ae90ec3ba0d4e5b72b77bd507cfbdb0d
Arthur Machado <arthur.machado@iclubs.tech>
2024-05-02 03:44:20 -0300

    feat: import all module to up application

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common'
 import { HttpModule } from './http/http.module'
+import { EnvModule } from './env/env.module'
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env'
 
 @Module({
-  imports: [HttpModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+    AuthModule,
+    HttpModule,
+  ],
 })
 export class AppModule {}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CreateClientController } from './controllers/create-client.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { DatabaseModule } from '../database/database.module'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [CreateClientController, AuthenticateController],
 })
 export class HttpModule {}
