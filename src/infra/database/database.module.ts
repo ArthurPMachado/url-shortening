c71e1c90c75957380373e1ae2eaa249ff52afac2
Arthur Machado <arthur.machado@iclubs.tech>
2024-05-02 22:35:24 -0300

    feat: add prisma short links to database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { IClientsRepository } from '@/domain/application/repositories/clients-repository'
 import { PrismaClientsRepository } from './prisma/repositories/prisma-clients-repository'
+import { IShortLinksRepository } from '@/domain/application/repositories/shortLinks-repository'
+import { PrismaShortLinksRepository } from './prisma/repositories/prisma-shortLinks-repository'
 
 @Module({
   providers: [
@@ -9,6 +11,10 @@ import { PrismaClientsRepository } from './prisma/repositories/prisma-clients-re
     {
       provide: IClientsRepository,
       useClass: PrismaClientsRepository,
+    },
+    {
+      provide: IShortLinksRepository,
+      useClass: PrismaShortLinksRepository,
     },
   ],
   exports: [PrismaService, IClientsRepository],
